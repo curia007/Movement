a9b79519d8f4b77d78ceb48026c23736c16933c6
Carmelo I. Uria <curia@carmelouria.com>
2020-07-01 13:35:00 -0600

    Initial Project

---
+++ Movement/Movement.h
@@ -0,0 +1,18 @@
+//
+//  Movement.h
+//  Movement
+//
+//  Created by Carmelo Uria on 7/1/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Movement.
+FOUNDATION_EXPORT double MovementVersionNumber;
+
+//! Project version string for Movement.
+FOUNDATION_EXPORT const unsigned char MovementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Movement/PublicHeader.h>
+
+
